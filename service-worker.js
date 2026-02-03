66938282406b36043a7e9a019ff36ba2858403a0
Maksim <130301328+MaksimBin@users.noreply.github.com>
2026-02-03 23:22:23 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'starship-neon-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './game.js',
+  './manifest.json',
+  './player.png',
+  './enemy.png',
+  './bg_tile.png',
+  './bg_music.mp3',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
